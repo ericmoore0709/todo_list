764e63a41fbe3e3fac6339c2abf3b427d6b3d3b3
Eric Moore <ericmoore0709@gmail.com>
2024-02-12 02:08:04 -0800

    Added localStorage support

--- index.js
+++ index.js
@@ -1,37 +1,51 @@
 function onFullyLoaded() {
 
     document.getElementById("task_list").addEventListener('click', (e) => {
-        // if the clicked item is an li...
-        if (e.target.tagName === "LI") {
-            // toggle class "checked"
+
+        if (e.target.tagName === "BUTTON") {
+            // ensure the user wishes to delete the task
+            if (!confirm("Are you sure you want to delete this task?"))
+                return;
+
+            // delete the li from the list
+            document.getElementById("task_list").removeChild(e.target.parentElement);
+        } else if (e.target.tagName === "LI") {
+            // if the clicked item is the LI text, toggle class "checked"
             e.target.classList.contains("checked") ? e.target.classList.remove("checked") : e.target.classList.add("checked")
         }
+
+        // add change to local storage
+        saveData();
+
     })
 
     document.querySelector("form").addEventListener("submit", (e) => {
         // prevent default submission
         e.preventDefault();
 
-        // get the input text
+        // get the input text (if it exists)
         const textValue = document.getElementById("task_text").value.trim();
-
-        // if there isn't any input text, don't bother.
         if (!textValue) return;
 
         // else create an li node to add
-        const newLi = document.createElement("li");
+        const newLi = createLi(textValue, false);
 
-        newLi.innerText = textValue;
-
-        // add li node
+        // add li node to tasklist
         document.getElementById("task_list").appendChild(newLi);
 
         // clear input text
         document.getElementById("task_text").value = ""
 
+        // add change to local storage
+        saveData();
+
     });
 
     document.getElementById("clear_tasks").addEventListener("click", () => {
+        // ensure tasks to be cleared
+        if (!confirm("Are you sure you want to clear all completed tasks? This action cannot be undone."))
+            return
+
         // for each li...
         document.querySelectorAll("li").forEach((li) => {
             // if li is checked...
@@ -40,38 +54,67 @@ function onFullyLoaded() {
                 document.getElementById("task_list").removeChild(li);
             }
         })
+
+        saveData();
+
     });
 
-    document.getElementById("save_config").addEventListener("click", () => {
-        const originalData = document.querySelectorAll("li");
-        const dataObj = [];
-        originalData.forEach((li) => {
-            dataObj.push({ text: li.innerText, checked: li.classList.contains("checked") });
-        })
-        localStorage.setItem("data", JSON.stringify(dataObj));
+    /**
+     * Updates the local storage data store with the current task list data representation
+     */
+    function saveData() {
+        const data = [];
+        const taskList = document.getElementById("task_list").childNodes;
+
+        taskList.forEach((li) => {
+
+            const obj = {
+                taskName: li.innerText.substring(0, li.innerText.length - 1),
+                checked: li.classList.contains("checked"),
+            }
+            data.push(obj);
+        });
+
+        localStorage.setItem("data", JSON.stringify(data));
+    }
+
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const data = JSON.parse(localStorage.getItem("data"));
+    const taskList = document.getElementById("task_list");
+
+    // for each task in data
+    data.forEach((task) => {
+
+        // create an li node to add
+        const newLi = createLi(task.taskName, task.checked);
+
+        // add li node to tasklist
+        taskList.appendChild(newLi);
     });
+})
 
-    document.getElementById("load_config").addEventListener("click", () => {
+/**
+ * Returns an li element with innerText text and checked class if checked.
+ * @param {*} text the innerText to set
+ * @param {*} checked whether to add checked class to li
+ * @returns the new li node
+ */
+function createLi(text, checked) {
+    const li = document.createElement("li");
+    li.innerText = text;
+    if (checked)
+        li.classList.add("checked");
 
-        if (!confirm("Are you sure you want to load the previous configuration? All unsaved data will be lost.")) {
-            return;
-        }
+    // add delete button
+    const delBtn = document.createElement("button");
+    delBtn.classList.add("delete_button");
+    delBtn.innerText = "X"
+    li.appendChild(delBtn);
 
-        const data = JSON.parse(localStorage.getItem("data"));
-        const taskList = document.getElementById("task_list");
+    return li;
 
-        // clear the to-do list as-is
-        while (taskList.firstChild) {
-            taskList.removeChild(taskList.firstChild);
-        }
-
-        data.forEach((li) => {
-            const newLi = document.createElement("li");
-            newLi.innerText = li.text;
-            if (li.checked) newLi.classList.add("checked");
-            taskList.appendChild(newLi);
-        })
-    });
 }
 
 window.addEventListener("load", onFullyLoaded);
